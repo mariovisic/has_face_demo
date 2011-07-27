c7946ca03389f9e19704c79c284a1bc765cd33fa
Mario Visic <mario@mariovisic.com>
2011-07-27 20:22:57 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HasFaceDemo::Application.config.secret_token = '492be8762a70e1cdd8b24f76786f1c0589bc28e6051375013d3b294db9b65c57c907d43982e459c7869a6711bcf31ad79967757c02831247761456a70094153d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HasFaceDemo::Application.config.session_store :cookie_store, :key => '_has_face_demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HasFaceDemo::Application.config.session_store :active_record_store
